1d3cbacabc6010342f8011e560707a4efee5f6b6
pyuvaratna <62106588+pyuvaratna@users.noreply.github.com>
2022-12-11 20:02:16 +0530

    update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,56 @@
 #!/bin/bash
+echo  -e "
 
+
+
+		 **    ** **     ** **      **     **    
+		//**  ** /**    /**/**     /**    ****   
+		 //****  /**    /**/**     /**   **//**  
+		  //**   /**    /**//**    **   **  //** 
+		   /**   /**    /** //**  **   **********
+		   /**   /**    /**  //****   /**//////**
+		   /**   //*******    //**    /**     /**
+		   //     ///////      //     //      //                   
+		     
+		     
+     
+"
 # Prompt user for domain input
 read -p "Enter a domain: " domain
+
+# Validate the domain name
+if ! [[ $domain =~ ^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$ ]]; then
+    echo  "Error: Invalid domain name"
+    exit 1
+fi
+
+# Check if the output files already exist, and prompt the user to confirm overwriting them
+output_files=(sub.txt livedomains.txt allUrls.txt liveallurls.txt dalfox.txt)
+for file in "${output_files[@]}"; do
+    if [ -f "$file" ]; then
+        read -p "$file already exists, do you want to overwrite it? [y/n] " yn
+        case $yn in
+            [Yy]* ) rm "$file";;
+            * ) echo "Cancelled" && exit;;
+        esac
+    fi
+done
 
 # Use ANSI escape codes to set the color of the messages
 echo -e "\n\e[32mFinding subdomains for $domain using subfinder...\n\e[0m"
 # Use subfinder to find subdomains for the given domain,
 # then write the output to sub.txt
-subfinder -d $domain -all -silent -o sub.txt
+subfinder -d "$domain" -all -silent -o sub.txt
 
 echo -e "\n\e[32mFinding live URLs for the subdomains using httprobe...\n\e[0m"
 # Use httprobe to find live URLs for the subdomains,
 # then write the output to livedomain.txt
-cat sub.txt | httprobe > livedomain.txt
+cat sub.txt | httprobe > livedomains.txt
 
 echo -e "\n\e[32mFinding URLs using gau...\n\e[0m"
 # Use gau to find URLs from the wayback machine,
 # then write the output to allUrls.txt
-cat livedomain.txt | gau --subs --blacklist png,jpg,gif,jpeg,swf,woff,gif,svg --o allUrls.txt
+cat livedomains.txt | gau --subs --blacklist png,jpg,gif,jpeg,swf,woff,gif,svg --o allUrls.txt
 
 echo -e "\n\e[32mFinding live URLs using httpx...\n\e[0m"
 # Use httpx to find live URLs from the wayback machine,
