88a2991bc27435437c162b4d36ef39e79b2765f6
pyuvaratna <62106588+pyuvaratna@users.noreply.github.com>
2022-12-16 13:14:45 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -14,14 +14,14 @@ echo  -e "
 read -p "Enter a domain: " domain
 
 # Make the new folder
-mkdir $domain
-cd $domain
+mkdir All
+cd All
 
 # Confirm that the folder was created
 if [ $? -eq 0 ]; then
-  echo "The $domain folder was successfully created."
+  echo "The All folder was successfully created."
 else
-  echo "There was an error creating the $folderName folder."
+  echo "There was an error creating the All folder."
 fi
 
 # Validate the domain name
@@ -76,7 +76,7 @@ echo -e "\n Successfully mixed in subdomains"
 
 
 
-echo -e "\n\e[32mFinding live URLs for the subdomains using httprobe...\n\e[0m"
+echo -e "\n\e[32mFinding live subdomains using httprobe...\n\e[0m"
 # Use httprobe to find live URLs for the subdomains,
 # then write the output to livedomain.txt
 cat subdomains.txt | httprobe -c 100 > livedomains.txt
@@ -88,18 +88,19 @@ if [ $? -ne 0 ]; then
 fi
 
 # Check for unique lines in the output file
-cat livedomains.txt| sort | uniq  | tee -a uniq_livedomains.txt
+cat livedomains.txt|sort | uniq  | tee -a uniq_livedomains.txt
 
 # Print a success message
-echo -e "\n Successfully checked unique lines in livedomains"
+echo -e "\n Successfully checked unique domains in livedomains"
 
 # Delete the input files
 rm sub1.txt sub2.txt
 
+
 echo -e "\n\e[32mFinding URLs using gau...\n\e[0m"
 # Use gau to find URLs from the wayback machine,
 # then write the output to allUrls.txt
-cat livedomains.txt | gau --verbose --subs --blacklist png,jpg,gif,jpeg,swf,woff,gif,svg --o allUrls.txt
+cat uniq_livedomains.txt | gau --verbose --subs --blacklist png,jpg,gif,jpeg,swf,woff,gif,svg --o allUrls.txt
 
 # Check the exit code of the previous command
 if [ $? -ne 0 ]; then
@@ -115,6 +116,21 @@ cat allUrls.txt | httpx -mc 200,403 -o liveallurls.txt
 echo -e "\n\e[32mFinding XSS vulnerabilities using dalfox...\n\e[0m"
 # Use dalfox to find XSS vulnerabilities,
 # then write the output to dalfox.txt
-cat liveallurls.txt | gf xss | sed 's/=.*/=/' | sed 's/URL: //' | sort -u | dalfox pipe
+cat liveallurls.txt | gf xss | sed 's/=.*/=/' | sed 's/URL: //' | sort -u | dalfox pipe |tee xss.txt
 
 echo -e "\n\e[32mProcess complete!\n\e[0m"
+
+#---------------------- Sending Results to Telegram --------------------------------#
+
+# Prompt user for domain input
+# Set your bot token and chat ID
+BOT_TOKEN=5599277543:AAH-D6I9GvzKtqs52B50wYHjA13aRtPVRWA
+CHAT_ID=-1001830541420
+
+
+
+# Set the path to the text file
+FILE_PATH=/home/kali/Desktop/All/xss.txt
+
+# Send the file to Telegram
+curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendDocument -F chat_id=$CHAT_ID -F document=@$FILE_PATH
